aec085ce9bd634e633a0c72d2d8aae6e89adc811
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-04-15 03:30:59 -0400

    linux-armv6: Specify system processor and add missing find root path.
    The additional find root path will ensure libraries like libm and libutils
    are found.

--- linux-armv6/Toolchain.cmake
+++ linux-armv6/Toolchain.cmake
@@ -1,5 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(cross_triple "arm-linux-gnueabihf")
 
@@ -7,7 +8,7 @@ set(CMAKE_C_COMPILER /usr/bin/${cross_triple}-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/${cross_triple}-g++)
 set(CMAKE_Fortran_COMPILER /usr/bin/${cross_triple}-gfortran)
 
-set(CMAKE_FIND_ROOT_PATH /usr/${cross_triple})
+set(CMAKE_FIND_ROOT_PATH /usr/${cross_triple} /usr/${cross_triple}/libc/usr)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
