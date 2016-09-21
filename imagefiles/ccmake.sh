c525ad106ea7f1af878eb9fecada0afc5d9b7bb8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-21 19:06:51 -0400

    cmake/ccmake: Provide a mechanism to disable passing of toolchain file
    By default the toolchain file is always passed as an option to cmake/ccmake
    
    It can be explicitly disabled setting the environment variable
    DOCKCROSS_PASS_CMAKE_TOOLCHAIN_FILE to 0

--- imagefiles/ccmake.sh
+++ imagefiles/ccmake.sh
@@ -3,4 +3,12 @@
 # Always pass the CMAKE_TOOLCHAIN_FILE variable to CMake when inside a
 # dockcross environment -- the CMAKE_TOOLCHAIN_FILE environmental variable is
 # always set in this context
-exec /usr/bin/ccmake -DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE} "$@"
+#
+# Passing of the option can be disabled setting environment variable
+# DOCKCROSS_PASS_CMAKE_TOOLCHAIN_FILE to 0
+
+if [ "${DOCKCROSS_PASS_CMAKE_TOOLCHAIN_FILE}" == "0" ];then
+  exec /usr/bin/ccmake "$@"
+else
+  exec /usr/bin/ccmake -DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE} "$@"
+fi
