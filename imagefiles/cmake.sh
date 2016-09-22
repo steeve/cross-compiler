4876f143565e5fe79f77e02923c5a7faca8657ee
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-09-21 21:11:33 -0400

    base/cmake: Do not pass toolchain option when using incompatible cmake option
    Also revert the changes to ccmake.sh, the restriction doesn't apply to it.
    
    Suggested-by: Matt McCormick <matt.mccormick@kitware.com>

--- imagefiles/cmake.sh
+++ imagefiles/cmake.sh
@@ -3,12 +3,20 @@
 # Always pass the CMAKE_TOOLCHAIN_FILE variable to CMake when inside a
 # dockcross environment -- the CMAKE_TOOLCHAIN_FILE environmental variable is
 # always set in this context
-#
-# Passing of the option can be disabled setting environment variable
-# DOCKCROSS_PASS_CMAKE_TOOLCHAIN_FILE to 0
 
-if [ "${DOCKCROSS_PASS_CMAKE_TOOLCHAIN_FILE}" == "0" ];then
-  exec /usr/bin/cmake "$@"
-else
-  exec /usr/bin/cmake -DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE} "$@"
-fi
+# Exception:
+#
+# Do not pass the toolchain when calling CMake with these options:
+#   -E                           = CMake command mode.
+#   --build <dir>                = Build a CMake-generated project binary tree.
+#   --find-package               = Run in pkg-config like mode.
+#
+case $1 in
+
+  -E|--build|--find-package)
+      exec /usr/bin/cmake "$@"
+      ;;
+
+esac
+
+exec /usr/bin/cmake -DCMAKE_TOOLCHAIN_FILE:FILEPATH=${CMAKE_TOOLCHAIN_FILE} "$@"
