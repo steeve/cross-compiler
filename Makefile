steeve/cross-compiler

commit 606d0b3ed91d0ffc7a13071174bf44736264534a

Makefile: Make sure BIN directory exist when running any test
This commit allows to run any test directly after checking out
the source code.

Makefile
@@ -84,7 +84,6 @@ base: Dockerfile
 	$(DOCKER) build -t $(ORG)/base .
 
 base.test: base
-	mkdir -p $(BIN)
 	$(DOCKER) run --rm dockcross/base > $(BIN)/dockcross-base && chmod +x $(BIN)/dockcross-base
 
 #
@@ -108,5 +107,13 @@ $(STANDARD_IMAGES): base
 $(addsuffix .test,$(STANDARD_IMAGES)): $$(basename $$@)
 	$(DOCKER) run --rm dockcross/$(basename $@) > $(BIN)/dockcross-$(basename $@) && chmod +x $(BIN)/dockcross-$(basename $@)
 	$(BIN)/dockcross-$(basename $@) python test/run.py $($@_ARGS)
+
+#
+# testing prerequisites implicit rule
+#
+test.prerequisites:
+	mkdir -p $(BIN)
+
+$(addsuffix .test,$(IMAGES)): test.prerequisites
 
 .PHONY: base images $(IMAGES) test %.test

commit 97385c1356dfcb8cb9d3725ae81c81768d9f47a4

Makefile: Make sure base.test also create bin directory
This commit fixes a regression [1] introduced in 606d0b3 (Makefile: Make sure
BIN directory exist when running any test)

Reported below the dry-run summary before and after, we can confirm the
bin directory is now created when building the `base.test` target.

Before:

$ make base.test --dry-run
docker build -t dockcross/base .
docker run --rm dockcross/base > bin/dockcross-base && chmod +x bin/dockcross-base

After:

$ make base.test --dry-run
docker build -t dockcross/base .
mkdir -p bin
docker run --rm dockcross/base > bin/dockcross-base && chmod +x bin/dockcross-base


[1] Error fixed by this commit:

Successfully built e6388fe49824
docker run --rm dockcross/base > bin/dockcross-base && chmod +x bin/dockcross-base
/bin/sh: 1: cannot create bin/dockcross-base: Directory nonexistent
make: *** [base.test] Error 2

make base.test returned exit code 2

Makefile
@@ -114,6 +114,6 @@ $(addsuffix .test,$(STANDARD_IMAGES)): $$(basename $$@)
 test.prerequisites:
 	mkdir -p $(BIN)
 
-$(addsuffix .test,$(IMAGES)): test.prerequisites
+$(addsuffix .test,base $(IMAGES)): test.prerequisites
 
 .PHONY: base images $(IMAGES) test %.test