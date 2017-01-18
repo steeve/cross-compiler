66a33a76474537c0b7c788583267a655277f5514
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-01-18 15:02:18 -0500

    manylinux-common: Fix pip install change ownership of "man" and "share" dir
    This commit will fix installation of packages like nose that would otherwise
    fail to install with error like the following:
    
      Installing collected packages: nose
      Exception:
      Traceback (most recent call last):
      [...]
      Permission denied: '/opt/_internal/cpython-2.7.11-ucs2/man'

--- manylinux-common/pre_exec.sh
+++ manylinux-common/pre_exec.sh
@@ -6,3 +6,10 @@ done
 for DIR in /opt/python/*/bin; do
   chown -R $BUILDER_UID:$BUILDER_GID $DIR
 done
+for DIR in /opt/python/*; do
+  mkdir $DIR/man
+  chown -R $BUILDER_UID:$BUILDER_GID $DIR/man
+done
+for DIR in /opt/python/*/share; do
+  chown -R $BUILDER_UID:$BUILDER_GID $DIR
+done
