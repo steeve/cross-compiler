7dd46fa0456c850f8613aa343fd562b6e40cfbfb
Dan Jacques <dnj@google.com>
2017-04-26 12:21:27 -0700

    Implement "ct-ng" building, add "linux-mips".
    Add a mechanism to construct a full cross-compiler environment using the
    "crosstool-ng" cross-compiler building utility. This is implemented in
    the new "common.crosstool" include, and augments the "dockcross/base"
    base image.
    
    Update Makefile to consolidate Dockerfile generation, notably the
    "sed"-based inclusion directives.
    
    Finally, employ all of this to generate a "linux-mips" 32-bit hard-float
    MIPS cross-compiler Dockcross image.

---
+++ linux-mips/Toolchain.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR mips)
+
+set(cross_triple "mips-unknown-linux-gnu")
+set(cross_root /usr/xcc/${cross_triple})
+
+set(CMAKE_C_COMPILER ${cross_root}/bin/${cross_triple}-gcc)
+set(CMAKE_CXX_COMPILER ${cross_root}/bin/${cross_triple}-g++)
+set(CMAKE_Fortran_COMPILER ${cross_root}/bin/${cross_triple}-gfortran)
+
+set(CMAKE_FIND_ROOT_PATH ${cross_root}/lib ${cross_root}/${cross_triple}/lib)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-mips)
