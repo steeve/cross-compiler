6fb4121ea9ebf3673574b343712e1f7d21a5316f
Nadav.Ruskin <nadav.ruskin@eyesight-tech.com>
2018-03-15 08:36:06 +0100

    Android: Use clang

--- android-arm/Toolchain.cmake
+++ android-arm/Toolchain.cmake
@@ -6,8 +6,8 @@ set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN /usr/${cross_triple}/)
 set(CMAKE_ANDROID_ARM_MODE 1)
 set(CMAKE_ANDROID_ARM_NEON 1)
 
-set(CMAKE_C_COMPILER /usr/${cross_triple}/bin/${cross_triple}-gcc)
-set(CMAKE_CXX_COMPILER /usr/${cross_triple}/bin/${cross_triple}-g++)
+set(CMAKE_C_COMPILER /usr/${cross_triple}/bin/${cross_triple}-clang)
+set(CMAKE_CXX_COMPILER /usr/${cross_triple}/bin/${cross_triple}-clang++)
 
 set(CMAKE_FIND_ROOT_PATH /usr/${cross_triple})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
