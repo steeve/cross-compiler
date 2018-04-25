88c828b855f15c9df84ece9764e4337727dab313
Nadav Ruskin <nadav.ruskin@eyesight-tech.com>
2018-04-25 05:50:45 -0400

    Trying to integrate my linux-arm64 solution to dockcross

--- linux-arm64/Toolchain.cmake
+++ linux-arm64/Toolchain.cmake
@@ -1,15 +1,21 @@
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(cross_triple "aarch64-linux-gnu")
+set(cross_triple "aarch64-unknown-linux-gnueabi")
 
-set(CMAKE_C_COMPILER /usr/bin/${cross_triple}-cc)
-set(CMAKE_CXX_COMPILER /usr/bin/${cross_triple}-c++)
-set(CMAKE_Fortran_COMPILER /usr/bin/${cross_triple}-gfortran)
+set(CMAKE_C_COMPILER /opt/x-tools/aarch64-unknown-linux-gnueabi/bin/${cross_triple}-cc)
+set(CMAKE_CXX_COMPILER /opt/x-tools/aarch64-unknown-linux-gnueabi/bin/${cross_triple}-c++)
+set(CMAKE_Fortran_COMPILER /opt/x-tools/aarch64-unknown-linux-gnueabi/bin/${cross_triple}-gfortran)
 
-# Discard path returned by pkg-config and associated with HINTS in module
-# like FindOpenSSL.
-set(CMAKE_IGNORE_PATH /usr/lib/x86_64-linux-gnu/ /usr/lib/x86_64-linux-gnu/lib/)
+set(CMAKE_FIND_ROOT_PATH /opt/x-tools/aarch64-unknown-linux-gnueabi)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_SYSROOT /opt/x-tools/aarch64-unknown-linux-gnueabi/aarch64-unknown-linux-gnueabi/sysroot)
+
+set(CMAKE_CXX_FLAGS "-I /opt/x-tools/aarch64-unknown-linux-gnueabi/include")
+set(ADDITIONAL_COMPILER_FLAGS "-L /usr/lib/aarch64-linux-gnu -Wl,-rpath-link,/usr/lib/aarch64-linux-gnu -Wl,-rpath-link,/lib/aarch64-linux-gnu")
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64)
