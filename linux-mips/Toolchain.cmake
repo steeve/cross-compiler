87d8255f03e11a74aed04599e98cd6b9b4cdebfd
Andre Richter <andre.o.richter@gmail.com>
2018-05-20 18:36:53 +0200

    Sync arm64 wtih other crosstool-ng targets

--- linux-mips/Toolchain.cmake
+++ linux-mips/Toolchain.cmake
@@ -9,9 +9,10 @@ set(CMAKE_C_COMPILER $ENV{CC})
 set(CMAKE_CXX_COMPILER $ENV{CXX})
 set(CMAKE_Fortran_COMPILER $ENV{FC})
 
-set(CMAKE_FIND_ROOT_PATH ${cross_root}/lib ${cross_root}/${cross_triple}/lib)
+set(CMAKE_FIND_ROOT_PATH ${cross_root} ${cross_root}/${cross_triple})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-mips)
