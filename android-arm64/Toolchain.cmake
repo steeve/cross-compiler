eb65a8970a2b166d67d192d93ac70083f38a3a6e
Anton Chernov <mechernov@gmail.com>
2018-05-25 18:34:20 +0200

    Added fortran compiler
    * Added FC to all Dockerfiles
    * Changed cmake toolchains to use environment variables for compilers

--- android-arm64/Toolchain.cmake
+++ android-arm64/Toolchain.cmake
@@ -2,18 +2,19 @@ set(CMAKE_SYSTEM_NAME Android)
 set(CMAKE_SYSTEM_VERSION 1)
 
 set(cross_triple aarch64-linux-android)
-set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN /usr/${cross_triple}/)
+set(CMAKE_ANDROID_STANDALONE_TOOLCHAIN $ENV{CROSS_ROOT})
 set(CMAKE_ANDROID_ARM_MODE 1)
 set(CMAKE_ANDROID_ARM_NEON 1)
 
-set(CMAKE_C_COMPILER /usr/${cross_triple}/bin/${cross_triple}-clang)
-set(CMAKE_CXX_COMPILER /usr/${cross_triple}/bin/${cross_triple}-clang++)
+set(CMAKE_C_COMPILER $ENV{CC})
+set(CMAKE_CXX_COMPILER $ENV{CXX})
+set(CMAKE_Fortran_COMPILER $ENV{FC})
 
-set(CMAKE_FIND_ROOT_PATH /usr/${cross_triple})
+set(CMAKE_FIND_ROOT_PATH $ENV{CROSS_ROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_SYSROOT /usr/${cross_triple}/sysroot)
+set(CMAKE_SYSROOT $ENV{CROSS_ROOT}/sysroot)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-arm)
 
