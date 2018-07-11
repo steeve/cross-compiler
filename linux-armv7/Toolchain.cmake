b3bcf24ffc3f539a0c542161c743ca56c2433514
Nadav Ruskin <nadav.ruskin@eyesight-tech.com>
2018-07-11 10:45:53 +0300

    Crosstool-ng'd armv7

--- linux-armv7/Toolchain.cmake
+++ linux-armv7/Toolchain.cmake
@@ -1,15 +1,20 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR arm)
-
-set(cross_triple "arm-linux-gnueabihf")
+set(cross_triple "armv7-unknown-linux-gnueabi")
+set(cross_root /usr/xcc/${cross_triple})
 
 set(CMAKE_C_COMPILER $ENV{CC})
 set(CMAKE_CXX_COMPILER $ENV{CXX})
 set(CMAKE_Fortran_COMPILER $ENV{FC})
 
-# Discard path returned by pkg-config and associated with HINTS in module
-# like FindOpenSSL.
-set(CMAKE_IGNORE_PATH /usr/lib/x86_64-linux-gnu/ /usr/lib/x86_64-linux-gnu/lib/)
+set(CMAKE_CXX_FLAGS "-I ${cross_root}/include/")
+
+set(CMAKE_FIND_ROOT_PATH ${cross_root} ${cross_root}/${cross_triple})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_SYSROOT ${cross_root}/${cross_triple}/sysroot)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-arm)
+
