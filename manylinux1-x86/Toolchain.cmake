a15af057530723f02222884f39e3eb2fd361b858
Jonas Vautherin <jonas.vautherin@gmail.com>
2019-06-12 02:43:44 +0200

    Manylinux toolchains: restore CMAKE_CROSSCOMPILING to FALSE after setting CMAKE_SYSTEM_NAME
    Then CMAKE_CROSSCOMPILING_EMULATOR (and therefore cross_triple) are not
    needed anymore.

--- manylinux1-x86/Toolchain.cmake
+++ manylinux1-x86/Toolchain.cmake
@@ -1,10 +1,9 @@
 set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CROSSCOMPILING FALSE)
 set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR i686)
 
 set(MANYLINUX1 TRUE)
-
-set(cross_triple "i686-linux-gnu")
 
 set(CMAKE_C_COMPILER /opt/rh/devtoolset-2/root/usr/bin/gcc)
 set(CMAKE_CXX_COMPILER /opt/rh/devtoolset-2/root/usr/bin/g++)
@@ -14,5 +13,3 @@ set(CMAKE_Fortran_COMPILER /opt/rh/devtoolset-2/root/usr/bin/gfortran)
 # Discard path returned by pkg-config and associated with HINTS in module
 # like FindOpenSSL.
 set(CMAKE_IGNORE_PATH /usr/lib/x86_64-linux-gnu/ /usr/lib/x86_64-linux-gnu/lib/)
-
-set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/${cross_triple}-noop)
