08c4f189cb31108c55f8d2b1f8111f6f7d38c213
mayeut <mayeut@users.noreply.github.com>
2021-04-07 20:44:13 +0200

    chore: update gosu 1.10 → 1.12
    changelog: https://github.com/tianon/gosu/compare/1.10...1.12

--- imagefiles/install-gosu-binary.sh
+++ imagefiles/install-gosu-binary.sh
@@ -13,7 +13,7 @@ if ! command -v gpg &> /dev/null; then
 	exit 1
 fi
 
-GOSU_VERSION=1.10
+GOSU_VERSION=1.12
 dpkgArch=$(if test $(uname -m) = "x86_64"; then echo amd64; else echo i386; fi)
 url="https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${dpkgArch}"
 url_key="https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${dpkgArch}.asc"
