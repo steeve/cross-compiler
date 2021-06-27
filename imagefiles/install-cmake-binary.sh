969f456ae063e70310ef1859800a201334d78647
Bensuperpc <bensuperpc@gmail.com>
2021-06-27 19:59:12 +0200

    Update CMake to 3.20.5 and git to 2.32.0, update install-cmake-binary.sh script
    Update CMake to 3.20.5 and git to 2.32.0, update install-cmake-binary.sh script
    
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- imagefiles/install-cmake-binary.sh
+++ imagefiles/install-cmake-binary.sh
@@ -35,10 +35,25 @@ fi
 
 cd /usr/src
 
-CMAKE_ROOT=cmake-${CMAKE_VERSION}-Centos5-${ARCH}
-url=https://github.com/dockbuild/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_ROOT}.tar.gz
+#Switch to newer version of CMake
+#USER=dockbuild #Original user, change it when changed it when there will be a new update of CMake (3.17.1 on June 27, 2021)
+
+USER=bensuperpc
+
+OS=Centos7
+
+CMAKE_ROOT=cmake-${CMAKE_VERSION}-${OS}-${ARCH}
+url=https://github.com/${USER}/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_ROOT}.tar.gz
+url_checksum=https://github.com/${USER}/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_ROOT}.tar.gz.sha256
+
 echo "Downloading $url"
 curl -# -LO $url
+
+echo "Downloading $url_checksum"
+curl -# -LO $url_checksum
+
+#Checksum package
+sha256sum -c ${CMAKE_ROOT}.tar.gz.sha256
 
 tar -xzvf ${CMAKE_ROOT}.tar.gz
 rm -f ${CMAKE_ROOT}.tar.gz
