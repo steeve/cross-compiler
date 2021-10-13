749fb64f2d2416c7c255e7c6a6b1ff59cb75b907
Bensuperpc <bensuperpc@gmail.com>
2021-10-13 19:44:08 +0200

    Update scripts
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- tools/dockcross-make-builder.sh
+++ tools/dockcross-make-builder.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 if (( $# >= 1 )); then
-    image=$1
-    build_file=build-${image%:*}
+    image_complet=$1
+    image=${image_complet%:*}
+    tag=${image_complet#*:}
+    build_file=build-$image
     shift 1
 
-    make_arg=$@
+    make_arg=$*
     echo "make arg: $make_arg"
 
-    #echo "Pulling dockcross/$image"
-    #docker pull dockcross/"$image"
+    echo "Pulling dockcross/$image"
+    docker pull "dockcross/$image:$tag"
 
     echo "Make script dockcross-$image"
     docker run --rm dockcross/"$image" > ./dockcross-"$image"
@@ -18,6 +20,6 @@ if (( $# >= 1 )); then
     echo "Build $build_file"
     ./dockcross-"$image" bash -c 'make CXX=${CXX} CC=${CC} AR=${AR} AS=${AS} LD=${LD} CPP=${CPP} FC=${FC} '"$make_arg"
 else
-    echo "Usage: ${0##*/} <docker imag (ex: linux-x64/linux-x64-clang/linux-arm64/windows-shared-x64/windows-static-x64...)> <make arg.>"
+    echo "Usage: ${0##*/} <docker image (ex: linux-x64/linux-x64-clang/linux-arm64/windows-shared-x64/windows-static-x64...)> <make arg.>"
     exit 1
 fi
