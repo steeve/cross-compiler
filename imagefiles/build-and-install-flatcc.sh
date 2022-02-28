1e8845e78e852a507d3b479f113cbce743e15d69
larry.espenshade <larry.espenshade@zetier.com>
2022-02-28 13:54:59 -0500

    add flatcc to Dockcross base image

---
+++ imagefiles/build-and-install-flatcc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+if ! command -v git &> /dev/null; then
+	echo >&2 'error: "git" not found!'
+	exit 1
+fi
+
+if [[ "${FLATCC_VERSION}" == "" ]]; then
+  echo >&2 'error: FLATCC_VERSION env. variable must be set to a non-empty value'
+  exit 1
+fi
+
+cd /usr/src
+
+git clone https://github.com/dvidelabs/flatcc.git flatcc -b v$FLATCC_VERSION --depth 1
+
+cd flatcc
+
+cmake -DFLATCC_INSTALL=on && make install > /dev/null
+
+./scripts/cleanall.sh
