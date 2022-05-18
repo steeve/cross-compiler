c74798ebb704edb092204b927ac14a53c9715934
Bensuperpc <bensuperpc@gmail.com>
2022-05-18 17:45:56 +0200

    Update cmake and ninja version
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- imagefiles/build-and-install-ninja.sh
+++ imagefiles/build-and-install-ninja.sh
@@ -25,14 +25,20 @@ while [ $# -gt 0 ]; do
   shift
 done
 
+if [[ -z "${NINJA_VERSION}" ]]; then
+  echo >&2 'error: NINJA_VERSION env. variable must be set to a non-empty value'
+  exit 1
+fi
+
 # Download
-REV=v1.10.2
+url="https://github.com/ninja-build/ninja/archive/${NINJA_VERSION}.tar.gz"
+
 curl --connect-timeout 30 \
     --max-time 10 \
     --retry 5 \
     --retry-delay 10 \
     --retry-max-time 30 \
-    -# -o ninja.tar.gz -LO https://github.com/ninja-build/ninja/archive/$REV.tar.gz
+    -# -o ninja.tar.gz -LO "v$url"
 
 mkdir ninja
 tar -xzvf ./ninja.tar.gz --strip-components=1 -C ./ninja
